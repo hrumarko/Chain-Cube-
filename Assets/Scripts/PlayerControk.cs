ebf8a2770a111e406f6a50465a82663c461fecf3
Marinov Vadym <ssadypic@gmail.com>
2022-06-13 14:58:13 +0300

    Init project

---
+++ Assets/Scripts/PlayerControk.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControk : MonoBehaviour, ICube
+{
+    public GameObject cube;
+    public GameObject cubeOutside;
+    public GameObject player;
+    public GameObject rightBound;
+    public GameObject leftBound;
+    public float speed = 10f;
+    public float speedForce = 50f;
+    bool endShoot;
+   
+    GameObject cubePrefab;
+    
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        SpawnCubes();
+        float maxZ = rightBound.transform.position.z;
+        float minZ = leftBound.transform.position.z;
+        rb = cubePrefab.GetComponent<Rigidbody>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        Move();
+        Shoot();
+        
+        
+        
+
+    }
+    public void Move(){
+       
+        Ray worldPosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float maxZ = rightBound.transform.position.z;
+        float minZ = leftBound.transform.position.z;
+        
+
+        float z = worldPosition.direction.z;
+        float x = cubePrefab.transform.position.x;
+        float y = cubePrefab.transform.position.y;
+              
+        Vector3 pos = new Vector3(x, y, z * speed);
+        
+        if(Input.GetMouseButton(0) ){
+            
+            
+            if(cubePrefab.transform.position.z <= maxZ & cubePrefab.transform.position.z >= minZ){
+            cubePrefab.transform.position = pos;
+            }
+            if(cubePrefab.transform.position.z > maxZ){
+                
+                Vector3 positionZ = new Vector3(x, y, maxZ);
+                cubePrefab.transform.position = positionZ;
+            }
+            if(cubePrefab.transform.position.z < minZ){
+                
+                Vector3 positionZ = new Vector3(x, y, minZ);
+                cubePrefab.transform.position = positionZ;
+            }
+        }
+        
+        
+    }
+    public void Shoot(){
+       if(Input.GetMouseButtonUp(0)){
+        Vector3 add = new Vector3(-10f, cubePrefab.transform.position.y, cubePrefab.transform.position.z);
+        rb.AddForce(Vector3.left * speedForce, ForceMode.Impulse);
+        cubePrefab.transform.parent = cubeOutside.transform;
+        
+        
+    }
+    
+    
+    }
+    public void SpawnCubes()
+        {
+            cubePrefab = Instantiate(cube, new Vector3(4, 0.91f, 0), Quaternion.identity);
+            cubePrefab.transform.parent = player.transform;
+        }
+}
